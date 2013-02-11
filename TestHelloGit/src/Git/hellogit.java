763d55f49d1d9d612750fb249570ebe129fcb3c2
neerajmind <neerajkumar_bhatewara@mindtree.com>
2013-02-11 11:35:14 +0530

    Test

---
+++ TestHelloGit/src/Git/hellogit.java
@@ -0,0 +1,17 @@
+package Git;
+
+public class hellogit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("some git stuff");
+		System.out.println("added from local workspace npp");
+		System.out.println("added from eclipse");
+		
+		
+	}
+
+}
