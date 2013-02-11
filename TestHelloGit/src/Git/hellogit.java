686f0d101f43f54ad4374513edbde06fb71f6323
neerajmind <neerajkumar_bhatewara@mindtree.com>
2013-02-11 15:46:48 +0530

    commint to conflict

--- TestHelloGit/src/Git/hellogit.java
+++ TestHelloGit/src/Git/hellogit.java
@@ -12,6 +12,7 @@ public class hellogit {
 		System.out.println("added from eclipse");
 		System.out.println("added from eclipse perconf ws(1st eclipse)");
 		System.out.println("added from test eclipse ws (2nd eclipse)");
+		System.out.println("to create a conflict added from 1st eclips without pull ");
 	}
 
 }
